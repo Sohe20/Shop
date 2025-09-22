e706d986a03abe9da157cb48c82b9e523c1edea6
soheil <aliasgharysoheil02@gmail.com>
2025-09-23 01:03:24 +0330

    feature : add csrf

--- app.js
+++ app.js
@@ -54,10 +54,10 @@ app.use((req, res, next) => {
 
 
 app.use((req,res,next)=>{
-
-    res.locals.isAuthenticated=req.session.isLoggedIn;
-    res.locals.csrfToken=req.csrfToken();
-    next();
+  res.locals.isAuthenticated = req.session.isLoggedIn;
+  res.locals.csrfToken = req.csrf();
+  next();
+  
 });
 
 
